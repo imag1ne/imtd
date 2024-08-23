b6b6faf1b7de30242859b8e819bcbfa71e0f8f10
Xudong Zhu <zhuxudon@gruenau2.informatik.hu-berlin.de>
2024-08-23 22:29:10 +0200

    calculate diatance

--- scripts/calculate_distance.py
+++ scripts/calculate_distance.py
@@ -3,38 +3,26 @@ import time
 import pm4py
 from pm4py.objects.log.util.xes import DEFAULT_NAME_KEY, DEFAULT_RESOURCE_KEY
 
-from imtd.algo.discovery.inductive.variants.im_bi.util.distance import distance_matrix
 from imtd import distance_matrix as distance_matrix_par
 
+# DESIRABLE_EVENT_LOG = '../Dataset/BPI Challenge 2017_1_all/desirable_event_log_sample_1000.xes'
+# UNDESIRABLE_EVENT_LOG = '../Dataset/BPI Challenge 2017_1_all/undesirable_event_log_sample_1000.xes'
+DESIRABLE_EVENT_LOG = '../Dataset/BPI Challenge 2017_1_all/BPI Challenge 2017 desirable.xes'
+UNDESIRABLE_EVENT_LOG = '../Dataset/BPI Challenge 2017_1_all/BPI Challenge 2017 undesirable.xes'
 
 def main():
-    event_log_plus = '../Dataset/BPI Challenge 2017_1_all/desirable_event_log_sample_1000.xes'
-    event_log_minus = '../Dataset/BPI Challenge 2017_1_all/undesirable_event_log_sample_1000.xes'
-    event_log_plus = pm4py.read_xes(event_log_plus, return_legacy_log_object=True)
-    event_log_minus = pm4py.read_xes(event_log_minus, return_legacy_log_object=True)
+    event_log_plus = pm4py.read_xes(DESIRABLE_EVENT_LOG, return_legacy_log_object=True)
+    event_log_minus = pm4py.read_xes(UNDESIRABLE_EVENT_LOG, return_legacy_log_object=True)
 
     start = time.time()
-    m1 = distance_matrix(event_log_plus, event_log_minus)
-    end = time.time()
-    print("Python implementation elapsed time: ", end - start)
-
-    event_log_plus_extracted = []
-    for trace in event_log_plus:
-        new_trace = []
-        for event in trace:
-            new_trace.append((event[DEFAULT_NAME_KEY], event[DEFAULT_RESOURCE_KEY]))
-        event_log_plus_extracted.append(new_trace)
-    event_log_minus_extracted = []
-    for trace in event_log_minus:
-        new_trace = []
-        for event in trace:
-            new_trace.append((event[DEFAULT_NAME_KEY], event[DEFAULT_RESOURCE_KEY]))
-        event_log_minus_extracted.append(new_trace)
-
-    start = time.time()
-    m2 = distance_matrix_par(event_log_plus_extracted, event_log_minus_extracted)
+    m2 = distance_matrix_par(event_log_plus, event_log_minus)
     end = time.time()
     print("Rust implementation elapsed time: ", end - start)
+
+def check_matrices(m1, m2):
+    for i in range(len(m1)):
+        for j in range(len(m1[i])):
+            assert m1[i][j] == m2[i][j]
 
 if __name__ == '__main__':
     main()
