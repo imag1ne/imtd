eebdd483d9c72c1eb882a5b310b3e2aa9f2cc972
imag1ne <zhuxudong1992@gmail.com>
2024-09-24 16:34:14 +0100

    sort grouped result

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
-from pathlib import Path
 import numpy as np
+import argparse
+from pathlib import Path
 
-DATA_PATH = Path('output/output_100_traces')
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='Plot', description='Plot')
+    parser.add_argument('-d', '--data-path', type=Path, required=True)
+
+    return parser.parse_args()
+
 
 def main():
+    args = parse_args()
+    data_path = args.data_path
     measurement_keys = {'acc', 'F1', 'precision', 'fitP', 'fitM', 'acc_perf', 'F1_perf', 'acc_ML', 'prc_ML', 'rec_ML'}
     imf_results = {key: [] for key in measurement_keys}
     imf_results['threshold'] = []
@@ -16,7 +25,7 @@ def main():
     imtd_results = {key: [] for key in measurement_keys}
     imtd_results['support'] = []
     imtd_results['ratio'] = []
-    for dirpath, dirnames, filenames in DATA_PATH.walk():
+    for dirpath, dirnames, filenames in data_path.walk():
         for filename in filenames:
             filepath = dirpath.joinpath(filename)
             parsed = parse_data_filename(filename)
@@ -48,15 +57,17 @@ def main():
     imbi_df = pd.DataFrame(imbi_results)
     imtd_df = pd.DataFrame(imtd_results)
 
-    plot_imf_results(imf_df)
-    plot_imbi_results(imbi_df)
-    plot_imtd_results(imtd_df)
+    plot_imf_results(imf_df, data_path)
+    plot_imbi_results(imbi_df, data_path)
+    plot_imtd_results(imtd_df, data_path)
+
 
 def as_float(n: str) -> float:
     try:
         return float(n)
     except ValueError:
         return np.nan
+
 
 def load_data_to_dict(filepath: Path, obj: dict, keys: set[str]) -> None:
     with open(filepath, 'r', newline='') as csvfile:
@@ -65,6 +76,7 @@ def load_data_to_dict(filepath: Path, obj: dict, keys: set[str]) -> None:
             key, value = row[0], as_float(row[1])
             if key in keys:
                 obj[key].append(value)
+
 
 def parse_data_filename(filename: str) -> tuple[str, list[float]] | None:
     filename_parts = filename.split('_')
@@ -80,14 +92,16 @@ def parse_data_filename(filename: str) -> tuple[str, list[float]] | None:
             threshold = float(filename_parts[2][1:4])
             return variant, [threshold]
         case 'imbi':
-            if len(filename_parts) < 4 or not filename_parts[2].startswith('s') or not filename_parts[3].startswith('r'):
+            if len(filename_parts) < 4 or not filename_parts[2].startswith('s') or not filename_parts[3].startswith(
+                    'r'):
                 return None
 
             support = float(filename_parts[2][1:])
             ratio = float(filename_parts[3][1:].rstrip('.csv'))
             return variant, [support, ratio]
         case 'imtd':
-            if len(filename_parts) < 4 or not filename_parts[2].startswith('s') or not filename_parts[3].startswith('r'):
+            if len(filename_parts) < 4 or not filename_parts[2].startswith('s') or not filename_parts[3].startswith(
+                    'r'):
                 return None
 
             support = float(filename_parts[2][1:])
@@ -96,7 +110,9 @@ def parse_data_filename(filename: str) -> tuple[str, list[float]] | None:
         case _:
             return None
 
+
 def plot_results(df, title, xlabel, xkey, savepath):
+    df = df.sort_values(by=xkey)
     plt.plot(df[xkey], df['precision'], label='Precision', color='red', marker='s', linestyle='-', linewidth=2)
     plt.plot(df[xkey], df['fitP'], label='Fitness', color='blue', marker='s', linestyle='-', linewidth=2)
     plt.plot(df[xkey], df['fitM'], label='Fitness_M', color='green', marker='s', linestyle='-', linewidth=2)
@@ -111,30 +127,34 @@ def plot_results(df, title, xlabel, xkey, savepath):
     plt.savefig(savepath, dpi=300)
     plt.clf()
 
-def plot_imf_results(imf_df):
-    plot_results(imf_df, 'IMF-BPIC17', 'f', 'threshold', DATA_PATH.joinpath('imf_fig.png'))
 
-def plot_imbi_results(imbi_df):
+def plot_imf_results(imf_df, savepath: Path):
+    plot_results(imf_df, 'IMF-BPIC17', 'f', 'threshold', savepath.joinpath('imf_fig.png'))
+
+
+def plot_imbi_results(imbi_df, savepath: Path):
     imbi_df_support_group = imbi_df.groupby('support')
     for support, df in imbi_df_support_group:
         plot_results(df, 'IMbi-BPIC17 (support={})'.format(support), 'ratio', 'ratio',
-                     DATA_PATH.joinpath('imbi_fig_s{}.png'.format(support)))
+                     savepath.joinpath('imbi_fig_s{}.png'.format(support)))
 
     imbi_df_ratio_group = imbi_df.groupby('ratio')
     for ratio, df in imbi_df_ratio_group:
         plot_results(df, 'IMbi-BPIC17 (ratio={})'.format(ratio), 'support', 'support',
-                     DATA_PATH.joinpath('imbi_fig_r{}.png'.format(ratio)))
+                     savepath.joinpath('imbi_fig_r{}.png'.format(ratio)))
 
-def plot_imtd_results(imtd_df):
+
+def plot_imtd_results(imtd_df, savepath: Path):
     imtd_df_support_group = imtd_df.groupby('support')
     for support, df in imtd_df_support_group:
         plot_results(df, 'IMtd-BPIC17 (support={})'.format(support), 'ratio', 'ratio',
-                     DATA_PATH.joinpath('imtd_fig_s{}.png'.format(support)))
+                     savepath.joinpath('imtd_fig_s{}.png'.format(support)))
 
     imtd_df_ratio_group = imtd_df.groupby('ratio')
     for ratio, df in imtd_df_ratio_group:
         plot_results(df, 'IMtd-BPIC17 (ratio={})'.format(ratio), 'support', 'support',
-                     DATA_PATH.joinpath('imtd_fig_r{}.png'.format(ratio)))
+                     savepath.joinpath('imtd_fig_r{}.png'.format(ratio)))
+
 
 if __name__ == "__main__":
     main()
