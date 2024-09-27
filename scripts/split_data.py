a69ad796db6245e0dcd5766d4e11ea738ee99ad1
imag1ne <zhuxudong1992@gmail.com>
2024-09-27 08:07:21 +0100

    fix

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -5,22 +5,26 @@ from pm4py.utils import sample_cases
 EVENT_LOG_PATH = "../../Dataset/BPI Challenge 2017_1_all/"
 EVENT_LOG_NAME = "BPI Challenge 2017.xes"
 
+
 def main():
     event_log = pm4py.read_xes(EVENT_LOG_PATH + EVENT_LOG_NAME)
 
     sizes = [100, 1000, 2000, 5000]
     samples = (sample_cases(event_log, size) for size in sizes)
-    
+
     for sample, size in zip(samples, sizes):
         desirable_event_log, undesirable_event_log = split_event_log(sample)
         pm4py.write_xes(desirable_event_log, EVENT_LOG_PATH + "desirable_event_log_sample_" + str(size) + ".xes")
         pm4py.write_xes(undesirable_event_log, EVENT_LOG_PATH + "undesirable_event_log_sample_" + str(size) + ".xes")
 
+
 def split_event_log(event_log):
-    desirable_event_log = pm4py.filter_trace_attribute_values(event_log, DEFAULT_NAME_KEY, ['W_Call incomplete files'], False)
-    undesirable_event_log = pm4py.filter_trace_attribute_values(event_log, DEFAULT_NAME_KEY, ['W_Call incomplete files'])
+    desirable_event_log = pm4py.filter_trace_attribute_values(event_log, DEFAULT_NAME_KEY, ['W_Call incomplete files'])
+    undesirable_event_log = pm4py.filter_trace_attribute_values(event_log, DEFAULT_NAME_KEY,
+                                                                ['W_Call incomplete files'], False)
 
     return (desirable_event_log, undesirable_event_log)
+
 
 if __name__ == "__main__":
     main()
